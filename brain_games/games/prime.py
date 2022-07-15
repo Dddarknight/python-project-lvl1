bfaafd743110e187f16e158701937a080e5bbe7a
Dddarknight <101993082+Dddarknight@users.noreply.github.com>
2022-07-15 09:21:03 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,8 +1,8 @@
 from random import randint
 
 
-DESCRIPTION_PRIME = (f'Answer "yes" if given number is prime. '
-                     f'Otherwise answer "no".')
+DESCRIPTION_PRIME = (
+    'Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
 def calculate_prime():
